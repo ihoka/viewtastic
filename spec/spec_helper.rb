3a4423bb3ddd2a1da32eaab8465e95a3ad796b23
Istvan Hoka <istvan.hoka@gmail.com>
2009-11-29 18:44:00 +0200

    Changed database name.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,7 @@ plugin_spec_dir = File.dirname(__FILE__)
 ActiveRecord::Base.logger = Logger.new(plugin_spec_dir + "/debug.log")
 
 databases = YAML::load(IO.read(plugin_spec_dir + "/db/database.yml"))
-ActiveRecord::Base.establish_connection(databases[ENV["DB"] || "sqlite3"])
+ActiveRecord::Base.establish_connection(databases[ENV["DB"] || "viewtastic"])
 
 ActiveRecord::Schema.verbose = false
 load(File.join(plugin_spec_dir, "db", "schema.rb"))
