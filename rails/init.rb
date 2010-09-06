954b023489fd9e8d3a1eda14cc914d989a405d40
oleg dashevskii <be9@be9.ru>
2010-09-06 12:47:23 +0800

    Change ActiveSupport::Dependencies.load_paths to .autoload_paths
    The former is deprecated in Rails 2.3.9.

--- rails/init.rb
+++ rails/init.rb
@@ -1,2 +1,2 @@
 require File.join(File.dirname(__FILE__), *%w[.. lib viewtastic])
-ActiveSupport::Dependencies.load_paths << File.join(Rails.root, *%w[app presenters])
+ActiveSupport::Dependencies.autoload_paths << File.join(Rails.root, *%w[app presenters])
